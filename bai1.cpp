a63e54ce3aa45ba93d97548fc19f2254562762d7
Binh <tribinhvt@gmail.com>
2026-08-21 09:01:07 +0700

    sua bai1

--- bai1.cpp
+++ bai1.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-    std::cout << "Hello, C++!" << std::endl;
+    std::cout << "==============================" << std::endl;
+    std::cout << "       MY FIRST C++ APP       " << std::endl;
+    std::cout << "==============================" << std::endl;
+    std::cout << "Name: Anna" << std::endl;
+    std::cout << "Course: OOP with C++" << std::endl;
+    std::cout << "I am ready to learn C++" << std::endl;
     return 0;
 }
