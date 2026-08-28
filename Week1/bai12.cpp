219cdbe2cb5dfed62559cc04614f94892ce2f698
Binh <tribinhvt@gmail.com>
2026-08-28 11:00:54 +0700

    Add bai1 to week1

---
+++ Week1/bai12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_AGE = 120;
+
+int main() {
+    string name;
+    int age;
+    cout <<"What is your name?";
+    cin >> name;
+    cout <<"How old are you?";
+    cin >> age;
+}   
