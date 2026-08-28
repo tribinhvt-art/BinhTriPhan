41234d257e701ab499f8204c2bd71ea48a660f58
Binh <tribinhvt@gmail.com>
2026-08-28 10:48:12 +0700

    Convert Week1 to folder and add bai1

---
+++ Week1/bai1.cpp
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+#include <iostream>
+
+int main()
+{
+    std::cout << "==============================" << std::endl;
+    std::cout << "       MY FIRST C++ APP       " << std::endl;
+    std::cout << "==============================" << std::endl;
+    std::cout << "Name: Anna" << std::endl;
+    std::cout << "Course: OOP with C++" << std::endl;
+    std::cout << "I am ready to learn C++" << std::endl;
+    return 0;
+=======
+#include <iostream>
+
+int main()
+{
+    std::cout << "==============================" << std::endl;
+    std::cout << "       MY FIRST C++ APP       " << std::endl;
+    std::cout << "==============================" << std::endl;
+    std::cout << "Name: Anna" << std::endl;
+    std::cout << "Course: OOP with C++" << std::endl;
+    std::cout << "I am ready to learn C++" << std::endl;
+    return 0;
+>>>>>>> 82fa26e (Add C++ exercises)
+}
