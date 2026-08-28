8d5e7f38270fe1e9fa16b2fedbec8e49066149d1
Binh <tribinhvt@gmail.com>
2026-08-28 11:03:07 +0700

    Add bai1 to week1

---
+++ Week1/bai14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_AGE = 120;
+
+int main() {
+    cout << "==========================\n";
+    cout << "     MY FIRST C++ APP     \n";
+    cout << "==========================\n";
+
+    cout << "Name: Anna\n";
+    cout << "Course: OOP with C++\n";
+    cout << "School: HCMUTE\n";
+}   
