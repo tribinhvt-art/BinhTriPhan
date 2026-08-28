5c5785de3d2f2877799caf86466eb83f6f53e031
Binh <tribinhvt@gmail.com>
2026-08-28 11:11:15 +0700

    Add Array exercise

---
+++ week2/Array.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+const int N = 20;
+string names[N] = {
+    "Nguyen Minh Anh",
+    "Tran Quoc Bao",
+    "Le Hoang Nam",
+    "Pham Gia Huy",
+    "Vo Thanh Tung",
+    "Nguyen Duc Anh",
+    "Tran Minh Khoa",
+    "Le Quang Huy",
+    "Pham Tuan Kiet",
+    "Hoang Minh Quan",
+    "Nguyen Ngoc Linh",
+    "Tran Thao Vy",
+    "Le Bao Chau",
+    "Pham Minh Thu",
+    "Vo Ngoc Han",
+    "Nguyen Thanh Dat",
+    "Tran Gia Bao",
+    "Le Minh Tri",
+    "Pham Quoc Khanh",
+    "Vo Hoang Long"
+};
+
+string ids[N] = {
+    "25145001",
+    "25145002",
+    "25145003",
+    "25145004",
+    "25145005",
+    "25145006",
+    "25145007",
+    "25145008",
+    "25145009",
+    "25145010",
+    "25145011",
+    "25145012",
+    "25145013",
+    "25145014",
+    "25145015",
+    "25145016",
+    "25145017",
+    "25145018",
+    "25145019",
+    "25145020"
+};
+
+string phones[N] = {
+    "0901000001",
+    "0901000002",
+    "0901000003",
+    "0901000004",
+    "0901000005",
+    "0901000006",
+    "0901000007",
+    "0901000008",
+    "0901000009",
+    "0901000010",
+    "0901000011",
+    "0901000012",
+    "0901000013",
+    "0901000014",
+    "0901000015",
+    "0901000016",
+    "0901000017",
+    "0901000018",
+    "0901000019",
+    "0901000020"
+};
