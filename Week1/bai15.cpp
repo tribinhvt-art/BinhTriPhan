169be592a2fdbefbee7189f7b12046b7f63942fc
Binh <tribinhvt@gmail.com>
2026-08-28 11:03:37 +0700

    Add bai1 to week1

---
+++ Week1/bai15.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_AGE = 120;
+
+int main() {
+    cout << "==========================\n";
+    cout << "       STUDENT LIST       \n";
+    cout << "==========================\n";
+    //student1
+    cout << "Name: Nguyen Van A \n";
+    cout << "Course: 23110001\n";
+    cout << "School: 0901234567\n";
+    //student2
+    cout << "Name: Nguyen Van A \n";
+    cout << "Course: 23110001\n";
+    cout << "School: 0901234567\n";
+    //student3
+    cout << "Name: Nguyen Van A \n";
+    cout << "Course: 23110001\n";
+    cout << "School: 0901234567\n";
+    cout << "==========================\n";
+    cout << "Total students: 20\n";
+    cout << "==========================\n";
+}   
