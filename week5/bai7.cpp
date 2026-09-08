0b152211b869d7d327454b45562f4f07c8e7c2c0
Binh <tribinhvt@gmail.com>
2026-09-08 22:39:47 +0700

    Update code

---
+++ week5/bai7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int gcd = 1;
+
+    cout << "Nhap a: ";
+    cin >> a;
+
+    cout << "Nhap b: ";
+    cin >> b;
+
+    for (int i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0) {
+            gcd = i;
+        }
+    }
+
+    cout << "Uoc chung lon nhat: " << gcd;
+
+    return 0;
+}
