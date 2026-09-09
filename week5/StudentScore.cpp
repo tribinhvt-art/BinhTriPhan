3a5a14d7e13bbf856571bc1180e2048f77d06d35
Binh <tribinhvt@gmail.com>
2026-09-09 09:17:39 +0700

    update code

--- week5/StudentScore.cpp
+++ week5/StudentScore.cpp
@@ -5,8 +5,17 @@ int main() {
     int N = 0;
      
     cout << "How many students? ";
+
+    // Checking if valid
     if (N > 2 && N < 20) {
       cout << "Valid";
+
+      int studentPoints[N];
+      int i;
+      for ( i = 0; i < N ; i++ ) {
+        cout << "Nhap Diem studen " << i ;
+        cin >> studentPoints[i];
+      }
       
     } else { cout << "Invalid must be between 2 and 20 "; }
 }
