fb6c263493030ee8d4d18033a5457e0bc0edfb94
Binh <tribinhvt@gmail.com>
2026-09-10 22:41:23 +0700

    Update code

--- week5/StudentScore2.cpp
+++ week5/StudentScore2.cpp
@@ -171,6 +171,133 @@ void searchScore(double studentPoints[], int N) {
     }
 }
 
+void firstFailingStudent(double studentPoints[], int N) {
+
+    bool found = false;
+
+    for (int i = 0; i < N; i++) {
+
+        if (studentPoints[i] < 5) {
+
+            cout << "\nFirst failing student:\n";
+
+            cout << "Student "
+                 << i + 1
+                 << ": "
+                 << studentPoints[i]
+                 << "\n";
+
+            found = true;
+
+            break;
+        }
+    }
+
+    if (found == false) {
+        cout << "\nAll students pass!\n";
+    }
+}
+
+
+void compareAdjacent(double studentPoints[], int N) {
+
+    cout << "\nImproved students:\n";
+
+    for (int i = 1; i < N; i++) {
+
+        if (studentPoints[i] > studentPoints[i - 1]) {
+
+            cout << "Student "
+                 << i + 1
+                 << " improved compared to Student "
+                 << i
+                 << "\n";
+        }
+    }
+}
+
+void longestPassStreak(double studentPoints[], int N) {
+
+    int currentStreak = 0;
+    int longestStreak = 0;
+
+    for (int i = 0; i < N; i++) {
+
+        if (studentPoints[i] >= 5) {
+
+            currentStreak++;
+        }
+
+        else {
+
+            currentStreak = 0;
+        }
+
+        if (currentStreak > longestStreak) {
+
+            longestStreak = currentStreak;
+        }
+    }
+
+    cout << "\nLongest pass streak: "
+         << longestStreak
+         << " students\n";
+}
+
+void scoreDistribution(double studentPoints[], int N) {
+
+    int range1 = 0;
+    int range2 = 0;
+    int range3 = 0;
+    int range4 = 0;
+    int range5 = 0;
+
+    for (int i = 0; i < N; i++) {
+
+        if (studentPoints[i] < 5) {
+
+            range1++;
+        }
+
+        else if (studentPoints[i] < 6.5) {
+
+            range2++;
+        }
+
+        else if (studentPoints[i] < 8) {
+
+            range3++;
+        }
+
+        else if (studentPoints[i] < 9) {
+
+            range4++;
+        }
+
+        else {
+
+            range5++;
+        }
+    }
+
+    cout << "\n===== SCORE DISTRIBUTION =====\n";
+
+    cout << "0 - <5   : "
+         << range1 << "\n";
+
+    cout << "5 - <6.5 : "
+         << range2 << "\n";
+
+    cout << "6.5 - <8 : "
+         << range3 << "\n";
+
+    cout << "8 - <9   : "
+         << range4 << "\n";
+
+    cout << "9 - 10   : "
+         << range5 << "\n";
+}
+
 int main() {
     int N = 0;
      
@@ -224,6 +351,10 @@ int main() {
 
       secondHighest( studentPoints,  N);
       scoreFrequency(studentPoints, N);
+      firstFailingStudent( studentPoints, N );
+      compareAdjacent( studentPoints, N);
+      longestPassStreak(studentPoints, N)
+      scoreDistribution( studentPoints, N)
       
     } else { cout << "Invalid must be between 2 and 20 "; }
 }
